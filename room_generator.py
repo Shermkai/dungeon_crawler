c3db7ccefb77cfb04fedf605ea201f29e931f867
Shermkai <kaishermy@gmail.com>
2024-07-25 21:37:06 -0500

    Tweaked debug text

--- room_generator.py
+++ room_generator.py
@@ -56,14 +56,16 @@ while True:
         if message.decode() == 'Q':
             break
 
-        print("[RG] Returning data to client...")
         if message.decode() == 'RM1':     # Room message part 1
             description_reply, item_reply = generate_room(True)
+            print("[RG] Sent to client:", description_reply)
             socket.send_string(description_reply)
         elif message.decode() == 'RM2':   # Room message part 2
             description_reply, item_reply = generate_room(False)
+            print("[RG] Sent to client:", description_reply)
             socket.send_string(description_reply)
         elif message.decode() == 'ITEM':  # Room item
+            print("[RG] Sent to client:", item_reply)
             socket.send_string(item_reply)
 
 print("[RG] Closed room_generator.py")
