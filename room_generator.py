512b98faf7c8b8ecf5951440b439b43053a08355
Shermkai <kaishermy@gmail.com>
2024-08-05 21:40:18 -0500

    Swapped some items

--- room_generator.py
+++ room_generator.py
@@ -19,7 +19,7 @@ def generate_room(is_first_half):
                   'corridor', 'kitchen', 'dining room', 'ballroom', 'morgue', 'laboratory', 'recreational area',
                   'museum']
     clutter = ['cobwebs', 'ingots', 'bones', 'trash piles', 'art supplies', 'strange animal hairs']
-    items = ['key', 'sword', 'torch']
+    items = ['idol', 'diamond', 'torch']
     monsters = ['slime', 'skeleton']
     door_position = ['north', 'east', 'south', 'west']
 
